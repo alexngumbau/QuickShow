fa54a8cad53750c89287043db650aea0da7fb670
alexngumbau <alexngumbau48@gmail.com>
2026-03-07 16:43:25 +0300

    list bookings and shows

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -3,36 +3,35 @@ import { dummyShowsData } from "../../assets/assets";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
 import { dateFormat } from "../../lib/dateFormat";
-
+import { useAppContext } from "../../context/AppContext";
 
 const ListShows = () => {
+  const { axios, getToken, user } = useAppContext();
 
-  const currency = import.meta.env.VITE_CURRENCY
+  const currency = import.meta.env.VITE_CURRENCY;
 
   const [shows, setShows] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const getAllShows = async () => {
     try {
-      setShows([{
-        movie: dummyShowsData[0],
-        showDateTime: "2025-06-30T02:30:00.000Z",
-        showPrice: 59,
-        occupiedSeats: {
-          A1: "user_1",
-          B1: "user_2",
-          C1: "user_3",
-        }
-      }]);
+      const { data } = await axios.get("/api/admin/all-shows", {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+      setShows(data.shows);
       setLoading(false);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    getAllShows();
-  }, []);
+    if (user) {
+      getAllShows();
+    }
+  }, [user]);
 
   return !loading ? (
     <>
@@ -49,11 +48,19 @@ const ListShows = () => {
           </thead>
           <tbody className="text-sm font-light">
             {shows.map((show, index) => (
-              <tr key={index} className="border-b border-primary/10 bg-primary/5 even:bg-primary/10">
+              <tr
+                key={index}
+                className="border-b border-primary/10 bg-primary/5 even:bg-primary/10"
+              >
                 <td className="p-2 min-w-45 pl-5">{show.movie.title}</td>
                 <td className="p-2">{dateFormat(show.showDateTime)}</td>
-                <td className="p-2">{Object.keys(show.occupiedSeats).length}</td>
-                <td className="p-2">{currency} {Object.keys(show.occupiedSeats).length * show.showPrice}</td>
+                <td className="p-2">
+                  {Object.keys(show.occupiedSeats).length}
+                </td>
+                <td className="p-2">
+                  {currency}{" "}
+                  {Object.keys(show.occupiedSeats).length * show.showPrice}
+                </td>
               </tr>
             ))}
           </tbody>
@@ -62,7 +69,7 @@ const ListShows = () => {
     </>
   ) : (
     <Loading />
-  )
-}
+  );
+};
 
-export default ListShows
+export default ListShows;

83ea8f6145badda91403f0da50c6f1640d59e98d
alexngumbau <alexngumbau48@gmail.com>
2026-03-09 13:03:33 +0300

    made the pay now button interactive for payments which have not been completed

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -4,6 +4,7 @@ import Loading from "../components/Loading";
 import timeFormat from "../lib/timeFormat";
 import { dateFormat } from "../lib/dateFormat";
 import { useAppContext } from "../context/AppContext";
+import { Link } from "react-router-dom";
 
 const MyBookings = () => {
   const { axios, getToken, user, image_base_url } = useAppContext();
@@ -63,25 +64,22 @@ const MyBookings = () => {
             </div>
           </div>
 
-          <div className="flex flex-col md:item-end md:text-right justify-between p-4">
+          <div className="flex flex-col md:items-end md:text-right justify-between p-4">
             <div className="flex items-center gap-4">
-              <p className="text-2xl font-semibold mb-3">
-                {currency}
-                {item.amount}
-              </p>
-              {!item.isPaid && (
-                <button className="bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer">
+              <p className="text-2xl font-semibold mb-3">{currency}{item.amount}</p>
+              {!item.isPaid && 
+                <Link to={item.paymentLink} className="bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer">
                   Pay Now
-                </button>
-              )}
+                </Link>
+              }
             </div>
-            <div>
+            <div className="text-sm">
               <p>
-                <span className="text-gray-400">Total Tickets: </span>
+                <span className="text-gray-400">Total Tickets:</span>
                 {item.bookedSeats.length}
               </p>
               <p>
-                <span className="text-gray-400">Seat Numbers: </span>
+                <span className="text-gray-400">Seat Number: </span>
                 {item.bookedSeats.join(", ")}
               </p>
             </div>
